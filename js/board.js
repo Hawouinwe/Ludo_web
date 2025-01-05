ac0802b8b3c9659ef4394571afe654a723b80741
Hawouinwe <code@perso.fr>
2025-01-05 20:36:20 +0100

    Nettoyage fichier board.js

--- js/board.js
+++ js/board.js
@@ -1,4 +1,4 @@
-function relativeIdToAbsoluteId(nrel, color) { // case relative à une color -> case absolue
+function relativeIdToAbsoluteId(nrel, color) {
     switch(color) {
         case "red" :
             return (nrel + 40) % 52
@@ -9,116 +9,116 @@ function relativeIdToAbsoluteId(nrel, color) { // case relative à une color -> 
         case "green" :
             return (nrel + 27) % 52
     }
-} 
+}
 
 
 function idCaseAbs(nrel, color) {
     let n = relativeIdToAbsoluteId(nrel, color)
     switch (n) {
         case 0:
-            return "last_case_blue" ;
+            return "last_case_blue";
         case 1:
-            return "path_2" ;
+            return "path_2";
         case 2:
-            return "start_blue" ;
+            return "start_blue";
         case 3:
-            return "path_4" ;
+            return "path_4";
         case 4:
-            return "path_6" ;
+            return "path_6";
         case 5:
-            return "path_8" ;
+            return "path_8";
         case 6:
-            return "path_10" ;
+            return "path_10";
         case 7:
-            return "path_16" ;
+            return "path_16";
         case 8:
-            return "path_17" ;
+            return "path_17";
         case 9:
-            return "path_18" ;
+            return "path_18";
         case 10:
-            return "star_blue" ;
+            return "star_blue";
         case 11:
-            return "path_19" ;
+            return "path_19";
         case 12:
-            return "path_20" ;
+            return "path_20";
         case 13:
-            return "last_case_yellow" ;
+            return "last_case_yellow";
         case 14:
-            return "path_30" ;
+            return "path_30";
         case 15:
-            return "start_yellow" ;
+            return "start_yellow";
         case 16:
-            return "path_29" ;
+            return "path_29";
         case 17:
-            return "path_28" ;
+            return "path_28";
         case 18:
-            return "path_27" ;
+            return "path_27";
         case 19:
-            return "path_26" ;
+            return "path_26";
         case 20:
-            return "path_32" ;
+            return "path_32";
         case 21:
-            return "path_34" ;
+            return "path_34";
         case 22:
-            return "path_36" ;
+            return "path_36";
         case 23:
-            return "star_yellow" ;
+            return "star_yellow";
         case 24:
-            return "path_38" ;
+            return "path_38";
         case 25:
-            return "path_40" ;
+            return "path_40";
         case 26:
-            return "last_case_green" ;
+            return "last_case_green";
         case 27:
-            return "path_39" ;
+            return "path_39";
         case 28:
-            return "start_green" ;
+            return "start_green";
         case 29:
-            return "path_37" ;
+            return "path_37";
         case 30:
-            return "path_35" ;
+            return "path_35";
         case 31:
-            return "path_33" ;
+            return "path_33";
         case 32:
-            return "path_31" ;
+            return "path_31";
         case 33:
-            return "path_25" ;
+            return "path_25";
         case 34:
-            return "path_24" ;
+            return "path_24";
         case 35:
-            return "path_23" ;
+            return "path_23";
         case 36:
-            return "star_green" ;
+            return "star_green";
         case 37:
-            return "path_22" ;
+            return "path_22";
         case 38:
-            return "path_21" ;
+            return "path_21";
         case 39:
-            return "last_case_red" ;
+            return "last_case_red";
         case 40:
-            return "path_11" ;
+            return "path_11";
         case 41:
-            return "start_red" ;
+            return "start_red";
         case 42:
-            return "path_12" ;
+            return "path_12";
         case 43:
-            return "path_13" ;
+            return "path_13";
         case 44:
-            return "path_14" ;
+            return "path_14";
         case 45:
-            return "path_15" ;
+            return "path_15";
         case 46:
-            return "path_9" ;
+            return "path_9";
         case 47:
-            return "path_7" ;
+            return "path_7";
         case 48:
-            return "path_5" ;
+            return "path_5";
         case 49:
-            return "star_red" ;
+            return "star_red";
         case 50:
-            return "path_3" ;
+            return "path_3";
         case 51:
-            return "path_1" ;
+            return "path_1";
         default:
             console.log("Mauvais indice !");
     }
@@ -133,15 +133,15 @@ function idFinalPath(color, n) {
         case "red" :
             switch (n) {
                 case 1 :
-                    return "final_path_red_1" ;
+                    return "final_path_red_1";
                 case 2 :
-                    return "final_path_red_2" ;
+                    return "final_path_red_2";
                 case 3 :
-                    return "final_path_red_3" ;
+                    return "final_path_red_3";
                 case 4 :
-                    return "final_path_red_4" ;
+                    return "final_path_red_4";
                 case 5 :
-                    return "final_path_red_5" ;
+                    return "final_path_red_5";
                 default:
                     console.log("Mauvais indice final !");
             }
@@ -149,15 +149,15 @@ function idFinalPath(color, n) {
         case "blue" :
             switch (n) {
                 case 1 :
-                    return "final_path_blue_1" ;
+                    return "final_path_blue_1";
                 case 2 :
-                    return "final_path_blue_2" ;
+                    return "final_path_blue_2";
                 case 3 :
-                    return "final_path_blue_3" ;
+                    return "final_path_blue_3";
                 case 4 :
-                    return "final_path_blue_4" ;
+                    return "final_path_blue_4";
                 case 1 :
-                    return "final_path_blue_5" ;
+                    return "final_path_blue_5";
                 default:
                     console.log("Mauvais indice final !");
             }
@@ -165,15 +165,15 @@ function idFinalPath(color, n) {
         case "yellow" :
             switch (n) {
                 case 1 :
-                    return "final_path_yellow_1" ;
+                    return "final_path_yellow_1";
                 case 2 :
-                    return "final_path_yellow_2" ;
+                    return "final_path_yellow_2";
                 case 3 :
-                    return "final_path_yellow_3" ;
+                    return "final_path_yellow_3";
                 case 4 :
-                    return "final_path_yellow_4" ;
+                    return "final_path_yellow_4";
                 case 5 :
-                    return "final_path_yellow_5" ;
+                    return "final_path_yellow_5";
                 default:
                     console.log("Mauvais indice final !");
             }
@@ -181,15 +181,15 @@ function idFinalPath(color, n) {
         case "green" :
             switch (n) {
                 case 1 :
-                    return "final_path_green_1" ;
+                    return "final_path_green_1";
                 case 2 :
-                    return "final_path_green_2" ;
+                    return "final_path_green_2";
                 case 3 :
-                    return "final_path_green_3" ;
+                    return "final_path_green_3";
                 case 4 :
-                    return "final_path_green_4" ;
+                    return "final_path_green_4";
                 case 5 :
-                    return "final_path_green_5" ;
+                    return "final_path_green_5";
                 default:
                     console.log("Mauvais indice final !");
             }
@@ -200,82 +200,4 @@ function idFinalPath(color, n) {
 }
 
 
-function idHome(color, n) {
-    switch (color) {
-        case "red" :
-            switch (n) {
-                case 0 :
-                    return "subhome_red_0" ;
-                case 1 :
-                    return "subhome_red_1" ;
-                case 2 :
-                    return "subhome_red_2" ;
-                case 3 :
-                    return "subhome_red_3" ;
-                default:
-                    console.log("Mauvais indice subhome !");
-            }
-
-        case "blue" :
-            switch (n) {
-                case 0 :
-                    return "subhome_blue_0" ;
-                case 1 :
-                    return "subhome_blue_1" ;
-                case 2 :
-                    return "subhome_blue_2" ;
-                case 3 :
-                    return "subhome_blue_3" ;
-                default:
-                    console.log("Mauvais indice subhome !");
-            }
-
-        case "yellow" :
-            switch (n) {
-                case 0 :
-                    return "subhome_yellow_0" ;
-                case 1 :
-                    return "subhome_yellow_1" ;
-                case 2 :
-                    return "subhome_yellow_2" ;
-                case 3 :
-                    return "subhome_yellow_3" ;
-                default:
-                    console.log("Mauvais indice subhome !");
-            }
-
-        case "green" :
-            switch (n) {
-                case 0 :
-                    return "subhome_green_0" ;
-                case 1 :
-                    return "subhome_green_1" ;
-                case 2 :
-                    return "subhome_green_2" ;
-                case 3 :
-                    return "subhome_green_3" ;
-                default:
-                    console.log("Mauvais indice subhome !");
-            }
-        
-        default:
-            console.log("Mauvaise coloreur !");
-    }
-}
-
-// for(let i = 0 ; i < 4 ; i++) {
-//     document.getElementById(idHome("blue",i)).innerText = `${i}` ;
-// }
-
-
-
-
-
-
-function idCenter() {
-    return "center" ;
-}
-
 export { idCaseAbs, idFinalPath, relativeIdToAbsoluteId };
-
-
