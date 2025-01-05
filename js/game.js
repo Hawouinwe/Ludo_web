7f31a8a76e44f0e6fb77ef516d6fc2628dee32ea
Hawouinwe <code@perso.fr>
2025-01-05 20:47:25 +0100

    Nettoyage fichier game.js

--- js/game.js
+++ js/game.js
@@ -1,69 +1,57 @@
-
-//import { idCaseAbs } from "./board";
 import { Player, Pawn } from "./player.js"
 import { players, numPlayers, showCustomAlert, closeAlert, updateActivePlayer } from "./ui.js";
 import { idCaseAbs, relativeIdToAbsoluteId } from "./board.js";
 
-
-function movable(player, pawn, steps) { // Depend des règles choisies ?
+// Vérifie si un pion peut être déplacé
+function movable(player, pawn, steps) {
     if (player.pawns[pawn].position === 0) {
-        return false ;
+        return false;
     }
-    //console.log("ICI", player.isAtHome(pawn)) ;
-    //if (player.isAtHome(pawn) ) {
-    //    return false ;
-    //} 
-    console.log(`Chemin final : ${player.pawns[pawn].endPath}, endPos :${player.pawns[pawn].endPosition}, total : ${player.pawns[pawn].endPosition + steps}`) ;
+
+    // Vérifie si le pion ne dépasse pas la zone d'arrivée
     if (player.pawns[pawn].endPath === true && player.pawns[pawn].endPosition + steps > 6) {
-        return false ;
+        return false;
     }
-    return true ;
+    return true;
 }
 
-
+// Vérifie si une position est une case "étoile" (sûre)
 function isSafePos(pos, color) {
-    if(idCaseAbs(pos, color).includes("star")) {
-        return true ;
-    }
-    else {
-        return false ;
-    }
+    return idCaseAbs(pos, color).includes("star");
 }
 
+// Tente de manger un pion adverse sur la même case
 function tryAndEat(player, pawn) {
     if (player.pawns[pawn].endPath === true || player.pawns[pawn].hasFinished) {
-        console.log("Nope !") ;
-        return ;
+        return;
     }
-    const pos = relativeIdToAbsoluteId(player.pawns[pawn].position, player.color) ;
-    console.log(`pos : ${pos}`)
+    const pos = relativeIdToAbsoluteId(player.pawns[pawn].position, player.color);
+    
     if (isSafePos(player.pawns[pawn].position, player.color)) {
-        //console.log("La case est safe !") ;
-        return false ;
+        return false;
     }
-    let hasEaten = false ;
-    for(let i = 0 ; i < players.length ; i++) {
-        //console.log("a") ;
+
+    let hasEaten = false;
+    // Vérifie pour chaque joueur adverse s'il y a un pion à manger
+    for(let i = 0; i < players.length; i++) {
         if(players[i] !== player) {
-            //console.log(`Joueur ${players[i].color}`) ;
-            for(let p = 0 ; p < 4 ; p++) {
-                //console.log(`Pion ${p}`) ;
-                //console.log(`position : ${relativeIdToAbsoluteId(players[i].pawns[p].position, players[i].color)}`) ;
-                //console.log(`rel pos : ${players[i].pawns[p].position}, ${players[i].pawns[p].position !== 0}`) ;
-                if(players[i].pawns[p].position !== 0 && !players[i].pawns[p].hasFinished && !players[i].pawns[p].endPath && relativeIdToAbsoluteId(players[i].pawns[p].position, players[i].color) === pos) {
-                    players[i].enterStartZone(p) ;
-                    hasEaten = true ;
-                    //console.log(`${player.color} a mangé ${players[i].color} !`) ;
+            for(let p = 0; p < 4; p++) {
+                if(players[i].pawns[p].position !== 0 && !players[i].pawns[p].hasFinished && 
+                   !players[i].pawns[p].endPath && 
+                   relativeIdToAbsoluteId(players[i].pawns[p].position, players[i].color) === pos) {
+                    players[i].enterStartZone(p);
+                    hasEaten = true;
                 }
             }
         }
     }
-    return hasEaten ;
+    return hasEaten;
 }
 
+// Gère le tour d'un joueur
 async function play(player) {
-    let consecutiveSixes = 0 ;
-    let rollAgain = true ;
+    let consecutiveSixes = 0;
+    let rollAgain = true;
 
     const delay = ms => new Promise(res => setTimeout(res, ms));
 
@@ -72,82 +60,72 @@ async function play(player) {
     showCustomAlert(`${player.name}`, "À toi de jouer !")
 
     while (rollAgain) {
-        rollAgain = false ;
-        
-        //console.log("Avant attente") ;
-        
-        //console.log("Lancez le dé") ;
-        const dice = await player.dice.rollDice() ;
-        //console.log("Après attente") ;
+        rollAgain = false;
+        const dice = await player.dice.rollDice();
         
         if (dice === 6) {
+            // Logique pour un lancer de 6
             consecutiveSixes++;
-
-            const selectableForExit = [] ;
-            const selectableForMoving = [] ;
-        
-
-            for(let pawn = 0 ; pawn < 4 ; pawn++) {
+            const selectableForExit = [];    // Pions pouvant sortir
+            const selectableForMoving = [];  // Pions pouvant se déplacer
+            
+            for(let pawn = 0; pawn < 4; pawn++) {
                 if(player.pawns[pawn].isAtHome()) {
-                    selectableForExit.push(pawn) ;
+                    selectableForExit.push(pawn);
                 }
                 if (movable(player, pawn, dice)) {
-                    selectableForMoving.push(pawn) ;
+                    selectableForMoving.push(pawn);
                 }
             }
 
-            
-            let p = 0 ;
-
+            let p = 0;
 
             if (selectableForExit.length === 0) {
                 if (selectableForMoving.length === 0) {
                     // On ne fait rien
                 }
                 else if (selectableForMoving.length === 1) {
-                    p = selectableForMoving[0] ;
+                    p = selectableForMoving[0];
                 }
                 else {
                     p = await selectPawn([], selectableForMoving, player); // Remplace le prompt par click sur un pion
                 }
-                await player.move(p, dice) ;
-                const hasEaten = tryAndEat(player, p) ;
+                await player.move(p, dice);
+                const hasEaten = tryAndEat(player, p);
                 if (hasEaten) {
-                    rollAgain = true ;
+                    rollAgain = true;
                 }
                 if (player.pawns[p].hasFinished) {
-                    rollAgain = true ;
+                    rollAgain = true;
                 }
             }
             else if (selectableForMoving.length === 0) {
                 if (selectableForExit.length === 1) {
-                    p = selectableForExit[0] ;
+                    p = selectableForExit[0];
                 }
                 else {
                     p = await selectPawn(selectableForExit, [], player); // Remplace le prompt par click sur un pion
                 }
-                player.exitStartZone(p) ;
+                player.exitStartZone(p);
             }
             else {
                 p = await selectPawn(selectableForExit, selectableForMoving, player); // Remplace le prompt par click sur un pion
                 if (selectableForExit.includes(p)) {
-                    player.exitStartZone(p) ;
+                    player.exitStartZone(p);
                 } else {
-                    await player.move(p, dice) ;
-                    const hasEaten = tryAndEat(player, p) ;
+                    await player.move(p, dice);
+                    const hasEaten = tryAndEat(player, p);
                     if (hasEaten) {
-                        rollAgain = true ;
+                        rollAgain = true;
                     }
                     if (player.pawns[p].hasFinished) {
-                        rollAgain = true ;
+                        rollAgain = true;
                     }
                 }
             }
             
-            //console.log("Nouvelle position :", player.pawns[p].position) ;
-
             if (consecutiveSixes < 3) {
-                rollAgain = true ;
+                rollAgain = true;
             } else {
                 console.log('Three consecutive sixes - forfeiting turn.');
                 rollAgain = false;
@@ -155,15 +133,14 @@ async function play(player) {
                 break;
             }
             
-
         }
-
         else {
-            const selectable = [] ;
+            // Logique pour un lancer différent de 6
+            const selectable = [];
 
-            for(let pawn = 0 ; pawn < 4 ; pawn++) {
+            for(let pawn = 0; pawn < 4; pawn++) {
                 if (movable(player, pawn, dice)) {
-                    selectable.push(pawn) ;
+                    selectable.push(pawn);
                 }
             }
 
@@ -171,48 +148,45 @@ async function play(player) {
             let p = 0; 
 
             if (selectable.length === 0 ) {
-                //console.log("Aucun pion déplaceable") ;
+                //console.log("Aucun pion déplaceable");
             }
             else if (selectable.length === 1) {
-                p = selectable[0] ;
-                await player.move(p, dice) ;
-                const hasEaten = tryAndEat(player, p) ;
+                p = selectable[0];
+                await player.move(p, dice);
+                const hasEaten = tryAndEat(player, p);
                 if (hasEaten) {
-                    rollAgain = true ;
+                    rollAgain = true;
                 }
                 if (player.pawns[p].hasFinished) {
-                    rollAgain = true ;
+                    rollAgain = true;
                 }
             }
             else {
                 p = await selectPawn(selectable, [], player); // Remplace le prompt par click sur un pion
-                await player.move(p, dice) ;
-                const hasEaten = tryAndEat(player, p) ;
+                await player.move(p, dice);
+                const hasEaten = tryAndEat(player, p);
                 if (hasEaten) {
-                    rollAgain = true ;
+                    rollAgain = true;
                 }
                 if (player.pawns[p].hasFinished) {
-                    rollAgain = true ;
+                    rollAgain = true;
                 }
             }
-            
-
         }
-        //console.log("fin de la boucle while") ;
     }
-    //console.log("Fin du play") ;
 }
 
-
+// Permet au joueur de sélectionner un pion en le cliquant
 function selectPawn(selectableForExit, selectableForMoving, player) {
     return new Promise((resolve) => {
         const allSelectable = [...selectableForExit, ...selectableForMoving];
-
+        
+        // Met en surbrillance les pions sélectionnables et gère les clics
         allSelectable.forEach((pawnIndex) => {
             const pawnElement = player.pawns[pawnIndex].currentElement; 
-            console.log(`pawnElement : ${pawnElement}`) ;
+            console.log(`pawnElement : ${pawnElement}`);
             pawnElement.classList.add('highlight'); 
-            pawnElement.classList.add('highlight') ;
+            pawnElement.classList.add('highlight');
 
             const onClick = () => {
                 allSelectable.forEach((idx) => {
@@ -221,7 +195,7 @@ function selectPawn(selectableForExit, selectableForMoving, player) {
                     el.removeEventListener('click', onClick);
                 });
 
-                console.log(`${pawnIndex} choisi`) ;
+                console.log(`${pawnIndex} choisi`);
                 resolve(pawnIndex); 
             };
 
@@ -230,94 +204,34 @@ function selectPawn(selectableForExit, selectableForMoving, player) {
     });
 }
 
-
-/*
-function selectPawn(selectableForExit, selectableForMoving, player) {
-    return new Promise((resolve) => {
-        const allSelectable = [...selectableForExit, ...selectableForMoving];
-
-        function onClick(event) {
-            const selectedElement = event.currentTarget;
-
-            // Find the pawn index corresponding to the clicked element
-            const pawnIndex = player.pawns.findIndex(
-                (pawn) => pawn.currentElement === selectedElement
-            );
-
-            // Cleanup: Remove highlights and event listeners
-            allSelectable.forEach((idx) => {
-                const el = player.pawns[idx].currentElement;
-                el.classList.remove('highlight');
-                el.removeEventListener('click', onClick);
-            });
-
-            console.log(`${pawnIndex} choisi`);
-            resolve(pawnIndex);
-        }
-
-        // Highlight and add event listeners to selectable pawns
-        allSelectable.forEach((pawnIndex) => {
-            const pawnElement = player.pawns[pawnIndex].currentElement;
-            pawnElement.classList.add('highlight');
-            pawnElement.addEventListener('click', onClick);
-        });
-    });
-}
-*/
-
-async function gameLoop(n) {
-    // Fonction de test maintenant inutilisée
-    for (let i = 0; i < n; i++) {
-        console.log("joueur :" + (i % numPlayers) + "\n");
-
-        // Utilise `await` ici pour attendre chaque appel à `play` avant de passer à l'itération suivante
-        await play(players[i % numPlayers]);
-
-        console.log("----------------*-----------------\n");
-    }
-
-    console.log("FIN") ;
-}
-
-
-
+// Gère le déroulement complet de la partie
 async function game() {
-    let endGame = false ;
-    let i = 0 ;
+    let endGame = false;
+    let i = 0;
     let classment = [];
+    
     while (!endGame) {
-        //console.log("joueur :" + (i % numPlayers) + "\n");
         await play(players[i % numPlayers]);
-        let nb_victorious = 0 ;
-        for(let p = 0 ; p < players.length ; p++) {
+        // Vérifie les conditions de fin de partie
+        let nb_victorious = 0;
+        for(let p = 0; p < players.length; p++) {
             if (players[p].hasWon()) {
-                nb_victorious++ ;
+                nb_victorious++;
                 classment.push(players[p].name);
             }
         }
         if(nb_victorious == players.length - 1) {
-            endGame = true ;
+            endGame = true;
         }
-        i++ ;
+        i++;
     }
-    for (let p = 0; p < players.length; p++) {
-        if (!players[p].hasWon()) {
-            classment.push(players[p].name);
-        }
-    }
-    endOfGame(classment) ;
 }
 
+// Affiche le classement final
 function endOfGame(classment) {
-    showCustomAlert("Classement final !", classment, true) ;
+    showCustomAlert("Classement final !", classment, true);
 }
 
-
-game() ;
-
-//gameLoop(100);
-
-// Appel de la fonction asynchrone `gameLoop`
-
+game();
 
 export { game };
